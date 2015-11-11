abff3a4627d1bdfdb9ab7112ad20f97cf58d57fc
Gavin Lloyd <gavinhungry@gmail.com>
2015-11-10 17:32:06 -0800

    Replace method instead of replacing return value
    Closes #1

--- src/io/gavinhungry/xposed/noplaymusicfor/NoPlayMusicFor.java
+++ src/io/gavinhungry/xposed/noplaymusicfor/NoPlayMusicFor.java
@@ -1,10 +1,10 @@
 package io.gavinhungry.xposed.noplaymusicfor;
 
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
-import de.robv.android.xposed.XC_MethodHook;
-import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.XC_MethodReplacement;
 
 public class NoPlayMusicFor implements IXposedHookLoadPackage {
   public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
@@ -12,11 +12,11 @@ public class NoPlayMusicFor implements IXposedHookLoadPackage {
       try {
 
         XposedHelpers.findAndHookMethod("com.google.android.music.utils.ConfigUtils", lpparam.classLoader,
-          "isConciergeListenNowEnabled", new XC_MethodHook() {
+          "isConciergeListenNowEnabled", new XC_MethodReplacement() {
 
           @Override
-          protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-            param.setResult(false);
+          protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+            return false;
           }
         });
 
